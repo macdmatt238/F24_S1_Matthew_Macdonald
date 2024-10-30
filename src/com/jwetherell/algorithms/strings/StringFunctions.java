f41fc356f62e1816746f0a092bb0ad30e5a8cb9a
mmacdo55 <mmacdo55@uoguelph.ca>
2024-10-30 16:12:18 -0400

    reset

--- src/com/jwetherell/algorithms/strings/StringFunctions.java
+++ src/com/jwetherell/algorithms/strings/StringFunctions.java
@@ -13,9 +13,7 @@ public class StringFunctions {
     private static final char SPACE = ' ';
 
     public static final String reverseWithStringConcat(String string) {
-        if (string.equals(null)){
-            return String return_string;
-        }
+        
         String output = new String();
         for (int i = (string.length() - 1); i >= 0; i--) {
             output += (string.charAt(i));
@@ -24,10 +22,7 @@ public class StringFunctions {
     }
 
     public static final String reverseWithStringBuilder(String string) {
-
-        if (string.equals(null)){
-            return String return_string;
-        }
+        
         final StringBuilder builder = new StringBuilder();
         for (int i = (string.length() - 1); i >= 0; i--) {
             builder.append(string.charAt(i));
